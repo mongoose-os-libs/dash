76df16443998d9437c646404fa50d70ee1102d70
Наше вам с кисточкой <lsm@cesanta.com>
2017-09-01 11:52:54 +0100

    Make dash lib to use WS server
    PUBLISHED_FROM=0aad6de9d32d329843288dbcd3ad751dffb7dda9

--- src/mgos_dash.c
+++ src/mgos_dash.c
@@ -5,23 +5,50 @@
 #include "mgos_rpc.h"
 
 static void timer_cb(void *arg) {
-  char buf[40], *topic = get_cfg()->dash.topic;
-  if (topic == NULL) {
-    snprintf(buf, sizeof(buf), "/%s/dash", get_cfg()->device.id);
-    topic = buf;
-  }
-  struct mbuf m;
-  struct json_out out = JSON_OUT_MBUF(&m);
-  mbuf_init(&m, 0);
-  mgos_print_sys_info(&out);
-  bool ok = mgos_mqtt_pub(topic, m.buf, m.len, 1, false);
-  mbuf_free(&m);
-  LOG(LL_INFO, ("%d, %p", ok, arg));
+  const struct sys_config_dash *cfg = &get_cfg()->dash;
+  struct mg_rpc_call_opts opts = {.dst = mg_mk_str(cfg->server)};
+  mg_rpc_callf(mgos_rpc_get_global(), mg_mk_str("Dash.Heartbeat"), NULL, NULL,
+               &opts, "%M", (json_printf_callback_t) mgos_print_sys_info);
+  (void) arg;
+}
+
+static void s_debug_write_hook(enum mgos_hook_type type,
+                               const struct mgos_hook_arg *arg,
+                               void *userdata) {
+  const struct sys_config_dash *cfg = &get_cfg()->dash;
+  struct mg_rpc_call_opts opts = {.dst = mg_mk_str(cfg->server)};
+  static unsigned s_seq = 0;
+  mg_rpc_callf(mgos_rpc_get_global(), mg_mk_str("Dash.Log"), NULL, NULL, &opts,
+               "{fd:%d, data: %.*Q, t: %.3lf id:%Q, seq:%u}", arg->debug.fd,
+               (int) arg->debug.len, arg->debug.data, mg_time(),
+               get_cfg()->device.id, s_seq);
+  s_seq++;
+  (void) type;
+  (void) userdata;
 }
 
 bool mgos_dash_init(void) {
   const struct sys_config_dash *cfg = &get_cfg()->dash;
+
   if (!cfg->enable) return true;
+
+  struct mg_rpc_channel_ws_out_cfg chcfg = {
+    .server_address = mg_mk_str(cfg->server),
+#if MG_ENABLE_SSL
+    .ssl_ca_file = mg_mk_str(cfg->ca_file),
+#endif
+    .reconnect_interval_min = 5,
+    .reconnect_interval_max = 60,
+    .idle_close_timeout = 0,
+  };
+  struct mg_rpc_channel *ch = mg_rpc_channel_ws_out(mgos_get_mgr(), &chcfg);
+  if (ch == NULL) {
+    LOG(LL_ERROR, ("Cannot create dashboard connection"));
+    return false;
+  }
+  mg_rpc_add_channel(mgos_rpc_get_global(), chcfg.server_address, ch, true);
+  mgos_hook_register(MGOS_HOOK_DEBUG_WRITE, s_debug_write_hook, NULL);
+
   if (cfg->interval > 0) {
     mgos_set_timer(cfg->interval * 1000, 1, timer_cb, NULL);
     LOG(LL_INFO, ("Starting %d sec heartbeat timer", cfg->interval));
